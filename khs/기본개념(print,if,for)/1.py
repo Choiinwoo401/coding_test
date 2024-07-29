7fb663a45a75d5d8c8aa6308139118bfb63a7d73
hyeong717 <hyeong717@gmail.com>
2024-07-29 15:44:12 +0900

    정렬

--- khs/기본개념(print,if,for)/1.py
+++ khs/기본개념(print,if,for)/1.py
@@ -1,7 +1,8 @@
 import sys
-while True:
-    s = sys.stdin.readline().rstrip()
-    if s == '':
-        break
-    else:
-        print(s)
+N = int(input())
+array1 = [sys.stdin.readline().rstrip() for i in range(N)]
+array1 = list(set(array1))
+array1.sort()
+array1.sort(key=len)
+for i in array1:
+    print(i)
