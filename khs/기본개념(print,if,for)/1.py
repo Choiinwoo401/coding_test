986640c36b24456b90b4bfb80866d4aa4bccae63
hyeong717 <hyeong717@gmail.com>
2024-08-03 17:28:15 +0900

    스택, 큐, 덱

--- khs/기본개념(print,if,for)/1.py
+++ khs/기본개념(print,if,for)/1.py
@@ -1,19 +1,41 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-N = int(input().strip())
-array1 = list(map(int, input().split()))
-M = int(input().strip())
-array2 = list(map(int, input().split()))
-dict1 = {}
 
-for i in array1:
-    if i not in dict1:
-        dict1[i] = 1
-    else:
-        dict1[i] += 1
+N = int(input())
+deq1 = deque()
 
-for j in array2:
-    if j not in dict1:
-        print(0,end = ' ')
-    else:
-        print(dict1[j],end = ' ')
+for i in range(N):
+    order = input().split()
+    O = int(order[0])
+    if O == 1:
+        deq1.appendleft(order[1])
+    elif O == 2:
+        deq1.append(order[1])
+    elif O == 3:
+        if len(deq1) >= 1:
+            print(deq1.popleft())
+        else:
+            print(-1)
+    elif O == 4:
+        if len(deq1) >= 1:
+            print(deq1.pop())
+        else:
+            print(-1)
+    elif O == 5:
+        print(len(deq1))
+    elif O == 6:
+        if len(deq1) == 0:
+            print(1)
+        else:
+            print(0)
+    elif O == 7:
+        if len(deq1) >= 1:
+            print(deq1[0])
+        else:
+            print(-1)
+    elif O == 8:
+        if len(deq1) >= 1:
+            print(deq1[-1])
+        else:
+            print(-1)
